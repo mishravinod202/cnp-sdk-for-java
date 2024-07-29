77a6508a0f10e4081ee287c10b188588bda2373e
Nitinsl <100850276+Nitinsl@users.noreply.github.com>
2024-07-29 08:25:55 -0400

    Commiting version related changes. (#70)

--- src/main/java/io/github/vantiv/sdk/Versions.java
+++ src/main/java/io/github/vantiv/sdk/Versions.java
@@ -3,5 +3,5 @@ package io.github.vantiv.sdk;
 public class Versions {
 
     public static final String XML_VERSION="12.37";
-    public static final String SDK_VERSION="Java;12.37.3_17";
+    public static final String SDK_VERSION="Java;12.37.4_17";
 }
